7e2f4ca96b2243db6f7fd7ec20e6cc63aaf67159
Su Fong <oliversssf2@gmail.com>
2021-04-15 14:19:25 +0800

    15 apr 2021

--- greedy.py
+++ greedy.py
@@ -1,48 +1,102 @@
-
 import dplm_base
+import numpy as np 
 import matplotlib.pyplot as plt
+import csv
+import pandas as pd
 
-matplotlib.use('agg')
 
 plotting = True
 
-if __name__=='__main__':
+if __name__ == '__main__':
     dplm_instance = dplm_base.dplm('para1.csv')
     dplm_instance.show_dplm_config()
     dplm_instance.set_dplm_slot_num(20)
-    dplm_instance.set_dplm_spring_num(3)
-    dplm_instance.set_slot([-4, 13, 8])
-    dplm_instance.set_dplm_spring_constants([300,300,300])
-    dplm_instance.set_dplm_spring_lengths([0.2, 0.2, 0.2])
+    dplm_instance.set_dplm_spring_num(4)
+    # dplm_instance.set_slot([-4, 13, 8])
+    dplm_instance.set_dplm_spring_constants([600,300,250, 230])
+    dplm_instance.set_dplm_spring_lengths([0.1, 0.2, 0.17, .13])
     dplm_instance.set_dplm_allowed_angle_range(-40, 60, 1)
 
-    if plotting == True:
-        lower_limit, upper_limit, step_size, total_angle_num = dplm_instance.get_allowed_angle_range().values()
 
-        a,b,c, rmse = dplm_instance.calculate_current_moment()
-        # plt.cla()    
-        # plt.figure()
-        ax = plt.gca()
-        plt.plot(range(lower_limit, upper_limit+1), a, label = 'moment_weight', ls = '--', lw = 3, color = 'mediumaquamarine')
+    with open('greedy_graphs/4_springs/greedy.csv', mode='w+', newline='') as csvfile:
+        csvfile.writelines('number of springs: {}\n'.format(dplm_instance.get_spring_num()))
+        csvfile.writelines('Number of slots: {}\n'.format(dplm_instance.get_slot_num()))
+        csvfile.writelines('spring_constants: {}\n'.format(dplm_instance.get_spring_constatnts()))
+        csvfile.writelines('spring_lengths: {}\n'.format(dplm_instance.get_spring_init_lengths()))
+        
+        writer = csv.writer(csvfile, delimiter = ',', quotechar = '"')
+        # writer.writerow('"number of springs: {}"'.format(dplm_instance.get_spring_num()))
+        # writer.writerow('"Number of slots: {}"'.format(dplm_instance.get_slot_num()))
+        # writer.writerow('"spring_constants: {}"'.format(dplm_instance.get_spring_constatnts()))
+        # writer.writerow('"spring_lengths: {}"'.format(dplm_instance.get_spring_init_lengths()))
+        writer.writerow(['rmse', 'initial guess', 'final install positions'])
 
-        ax = plt.gca()
+        plt.ion()
+        fig = plt.figure(figsize=[9.6, 6.4])
+        # print(plt.gcf().number)
+        # fig2 = plt.figure(2)
+        # fig2, axs = plt.subplots(4,5)
+        # fig2.number = 2
+        # print(plt.gcf().number)
+        for sample in range(100): #try 100 different initial states
+            init_guess = np.random.randint(0, high=dplm_instance.get_slot_num()*2-1, size=dplm_instance.get_spring_num())-dplm_instance.get_slot_num()+1
+            guess = np.array(init_guess, copy=True)
+            for greedy_iter_num in range(1): #three iterations for each greedy
+                for ind in range(dplm_instance.get_spring_num()):
+                    rmse = np.zeros(dplm_instance.get_slot_num())
+                    for slot in range(-dplm_instance.get_slot_num()+1, dplm_instance.get_slot_num()):
+                        guess[ind] = slot
+                        dplm_instance.set_slot(guess)
+                        rmse[slot] = dplm_instance.current_rmse()
+                        guess[ind] = np.argmin(rmse)
+                dplm_instance.set_slot(guess)
 
-        for i in range(len(b)):
-            plt.plot(range(lower_limit,upper_limit+1), b[i], label = 'moment_spring_{}'.format(i+1), ls = '--', lw = 3, color = 'cornflowerblue')
+            if plotting == True:
+                plt.figure(1)
+                print(plt.gcf().number)
+                lower_limit, upper_limit, step_size, total_angle_num = dplm_instance.get_allowed_angle_range().values()
 
-        plt.plot(range(lower_limit, upper_limit+1), c, label = 'moment_total', ls = '--', lw = 3, color = 'mediumslateblue')
-        plt.axhline(y = 0, ls = '-', lw = 3, color = 'darkgrey')
+                a,b,c, rmse = dplm_instance.calculate_current_moment()
+                ax = plt.gca()
+                ax.cla()    
+                # plt.figure()
+                ax.plot(range(lower_limit, upper_limit+1), a, label = 'moment_weight', ls = '--', lw = 1, color = 'grey')
 
-        plt.axis(ymin=-20, ymax=50)
-        plt.legend()
-        plt.xlabel('angle [degree]')
-        plt.ylabel('moment [Nm]')
-        ax.xaxis.set_major_formatter('{x}°')
+                for i in range(len(b)):
+                    ax.plot(range(lower_limit,upper_limit+1), b[i], label = 'moment_spring_{}'.format(i+1), ls = '-', lw = 1, color = 'cornflowerblue')
+
+                ax.plot(range(lower_limit, upper_limit+1), c, label = 'moment_total', ls = '-', lw =4, color = 'gold')
+                ax.axhline(y = 0, ls = '-', lw = 3, color = 'darkgrey')
+
+                ax.axis(ymin=-20, ymax=50)
+                ax.legend()
+                plt.xlabel('angle [degree]')
+                plt.ylabel('moment [Nm]')
+                ax.xaxis.set_major_formatter("{x}°")
 
 
-        plt.text(-10,-10, r'$RMSE={:.2f}$'.format(rmse))
+                ax.text(-20,-13, 'RMSE={:.2f} \nInitial random state: {} \nFinal install positions: {}'.format(rmse,init_guess, guess))
+                plt.savefig('greedy_graphs/4_springs_t/test_{}.png'.format(sample+1))
+                # plt.show()
+                plt.pause(0.001)
+                # del fig
+                
+                writer.writerow([f'{rmse:.2f}', list(init_guess), list(guess)])
 
-        plt.savefig('test.png')
-        # plt.show()
+                print(rmse)
 
-        print(rmse)
+                # if (sample+1)%4==0:
+                #     plt.figure(2)
+                #     fig2.add_subplot(ax)
+
+        # plt.figure(2)
+
+    k = pd.read_csv('greedy_graphs/4_springs/greedy.csv', header = 4)
+
+    k.index = np.arange(1, len(k)+1)
+    k.index.name='sample'
+    k.to_excel('greedy_graphs/4_springs/greedy.xlsx')
+    k.to_csv('greedy_graphs/4_springs/greedy_processed.csv')
+    k.to_clipboard(sep = ',')
+
+    k
