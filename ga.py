7b652aa4f27660757b4d4e78a23a780fd2e6d0c5
fongsu <oliversssf2@gmail.com>
2021-04-17 02:51:05 +0800

    lkjkl

--- ga.py
+++ ga.py
@@ -29,7 +29,6 @@ def setup1(dplm_instance):
     dplm_instance.set_dplm_spring_num(3)
     dplm_instance.set_dplm_allowed_angle_range(-40, 60, 1)
 
-
     install_position_step = 1e-2
     spring_constant_step = 1e1
     spring_length_step = 1e-2
@@ -118,8 +117,40 @@ def setup4(file):
              variable_type_mixed=vartype)
     return model
 
+def setup5(dplm_instance): 
+    aps={'max_num_iteration': None,\
+                'population_size':100,\
+                'mutation_probability':0.2,\
+                'elit_ratio': 0.01,\
+                'crossover_probability': 0.6,\
+                'parents_portion': 0.3,\
+                'crossover_type':'uniform',\
+                'max_iteration_without_improv':30}
+
+    dplm_instance.show_dplm_config()
+    dplm_instance.set_dplm_slot_num(20)
+    dplm_instance.set_dplm_spring_num(3)
+    dplm_instance.set_dplm_allowed_angle_range(-40, 60, 1)
+
+    # install_position_step = 1e-2
+    spring_constant_step = 1e1
+    spring_length_step = 1e-2
+
+    # dplm_instance.set_dplm_spring_constants([400,300,200])
+    # dplm_instance.set_dplm_spring_lengths([0.2, 0.15, 0.1])
+    def f(X):
+        dplm_instance.set_slot(np.array([X[0],X[1],X[2]]))
+        dplm_instance.set_dplm_spring_lengths(np.array([X[3],X[4],X[5]])*spring_length_step)
+        dplm_instance.set_dplm_spring_constants(np.array([X[6],X[7],X[8]])*spring_constant_step)
+        return dplm_instance.current_rmse()
+
+    varbound=np.array([[-19,19]]*3+ [[0.2/spring_length_step,0.4/spring_length_step]]*3+ [[200/spring_constant_step, 400/spring_constant_step]]*3)
+    varbound = varbound.astype(int)
+    model=ga(function=f,dimension=9,variable_type='int',variable_boundaries=varbound, algorithm_parameters=aps)
+    return model
+
 # dplm_instance = dplm_base.dplm('para1.csv')
-model = setup4('para1.csv')
+model = setup5('para1.csv')
 
 
 start = time.time()
